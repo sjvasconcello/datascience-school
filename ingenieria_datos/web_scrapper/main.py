7a0833527fbd124fbd09ecec93b9b43ca28f7c9f
Santiago Vasconcello <sjvasconcello@gmail.com>
2020-07-10 00:37:36 -0400

    :poop: Working in the scrapper
    I need to fix the scrapper if i want to catch the body of the articles

--- ingenieria_datos/web_scrapper/main.py
+++ ingenieria_datos/web_scrapper/main.py
@@ -1,11 +1,21 @@
+import re
+import datetime
+import csv
 import argparse
 import logging
+
 import news_page_objects as news
 from common import config
+
+# Requeste erros ---
+from requests.exceptions import HTTPError
+from urllib3.exceptions import MaxRetryError
 
 logging.basicConfig(level=logging.INFO)
 
 logger = logging.getLogger(__name__)
+is_well_formed_url = re.compile(r"^https?://.+/+")
+is_root_path = re.compile(r"^/.+$")
 
 
 def _new_scraper(news_site_uid):
@@ -14,8 +24,54 @@ def _new_scraper(news_site_uid):
     logging.info("Begginng scraper for {}".format(host))
     homepage = news.HomePage(news_site_uid, host)
 
+    articles = []
     for link in homepage.article_links:
-        print(link)
+        article = _fetch_article(news_site_uid, host, link)
+        if article:
+            logger.info("Article fetched!")
+            articles.append(article)
+            break
+
+    _save_articles(news_site_uid, articles)
+
+
+def _save_articles(news_site_uid, articles):
+    now = datetime.datetime.now().strftime("%%Y_%m_%d")
+    out_file_name = "{news_site_uid}_{datetime}_article.csv".format(
+        news_site_uid=news_site_uid,
+        datetime=now)
+    csv_headers = list(filter(lambda property: not property.startswith('_'), dir(articles[0])))
+
+    with open(out_file_name, mode="w+") as f:
+        writer = csv.writer(f)
+        writer.writerow(csv_headers)
+
+        for article in articles:
+            row = [str(getattr(article, prop)) for prop in csv_headers]
+            writer.writerow(row)
+
+
+def _fetch_article(news_site_uid, host, link):
+    logger.info("Start fetching article at {}".format(link))
+    article = None
+    try:
+        article = news.ArticlePage(news_site_uid, _build_link(host, link))
+    except (HTTOError, MaxRetryError):
+        logger.warning("Error while fetching the article", exc_info=False)
+
+    if article and not article.body:
+        logger.warning("Invalid article. There is no body")
+        return None
+    return article
+
+
+def _build_link(host, link):
+    if is_well_formed_url.match(link):
+        return link
+    elif is_root_path.match(link):
+        return"{}{}".format(host, link)
+    else:
+        return "{host}/{uri}".format(host=host, uri=link)
 
 
 if __name__ == "__main__":
