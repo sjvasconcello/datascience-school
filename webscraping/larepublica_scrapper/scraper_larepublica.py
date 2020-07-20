d61aa2498967a697b0c1c612bd095bcb87d4b7fc
Santiago Vasconcello <sjvasconcello@gmail.com>
2020-07-19 22:45:49 -0400

    :construction: Visualization in Python
    6-estadistica-python

--- webscraping/larepublica_scrapper/scraper_larepublica.py
+++ webscraping/larepublica_scrapper/scraper_larepublica.py
@@ -1,12 +1,39 @@
+import os
+import datetime
 import requests
 import lxml.html as html
 
-HOME_URL = 'http://www.larepublica.co/'
-XPATH_LINK_TO_ARTICLE = '//h2[@class="headline"]/a/@href'
+HOME_URL = 'https://www.larepublica.co/'
+XPATH_LINK_TO_ARTICLE = '//div[@class="news V_Title_Img"]/h2/a/@href'
 XPATH_TITLE = '//div[@class="mb-auto"]/h2/a/text()'
 XPATH_BODY = '//div[@class="html-content"]/p/text()'
 XPATH_SUMMARY = '//div[@class="html-content"]/p[not@class]/text()'
 
+def parse_notice(link, today):
+    try:
+        response = requests.get(link)
+        if response.status_code == 200:
+            notice = response.content.decode("utf-8")
+            parsed = html.fromstring(notice)
+            try:
+                title = parsed.xpath(XPATH_TITLE)[0]
+                title = title.replace('\"','')
+                summary = parsed.xpath(XPATH_SUMMARY)[0]
+                body = parsed.xpath(XPATH_BODY)
+            except:
+                return
+            with open(f'{today}/title.txt','w', encoding='utf-8') as f:
+                f.write(title)
+                f.write('\n\n')
+                f.write(summary)
+                f.write('\n\n')
+                for p in body:
+                    f.write(p)
+                    f.write('\n')
+        else:
+            raise ValueError(f"Error: {response.status_code}") 
+    except ValueError as ve:
+        print(ve)
 
 def parse_home():
     try:
@@ -16,6 +43,16 @@ def parse_home():
             parsed = html.fromstring(home)
             links_to_notices = parsed.xpath(XPATH_LINK_TO_ARTICLE)
             print(links_to_notices)
+            
+            today = datetime.date.today().strftime("%d-%m-%Y")
+            
+            if not os.path.isdir(today):
+                os.mkdir(today)
+                
+            for link in links_to_notices:
+                parse_notice(link, today)
+            
+            
         else:
             raise ValueError(f" Error: ", {response.status_code})
     except ValueError as ve:
